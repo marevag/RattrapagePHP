888dc8b8d39f26cef994f05c1105a3e87d63e035
unknown <mareva.gauss@ynov.com>
2021-02-16 12:51:34 +0100

    debut afficher produit

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProduitRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProduitController extends AbstractController
+{
+    /**
+     * @Route("/produit", name="produit")
+     */
+    public function index(ProduitRepository $repo): Response
+    {
+        return $this->render('produit/index.html.twig', [
+          'produits' => $repo -> findAll()
+        ]);
+    }
+}
